26b5d7b78bf8421a51d05be250300b83a8866182
Larsv94 <larsvolkers@hotmail.nl>
2014-09-19 12:38:56 +0200

    update

---
+++ core/src/com/FOS/Pixel/screens/GameScreen.java
@@ -0,0 +1,18 @@
+package com.FOS.Pixel.screens;
+
+import com.badlogic.gdx.Game;
+
+/**
+ * Created by Lars on 9/19/2014.
+ */
+public class GameScreen extends PixelScreen {
+
+    public GameScreen(Game game) {
+        super(game);
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+    }
+}
