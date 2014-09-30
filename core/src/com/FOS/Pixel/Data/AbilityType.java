784a49b63c43132b4ab5c5b3e95b124bc63553e7
Larsv94 <larsvolkers@hotmail.nl>
2014-09-30 10:24:54 +0200

    Json problem

---
+++ core/src/com/FOS/Pixel/Data/AbilityType.java
@@ -0,0 +1,12 @@
+package com.FOS.Pixel.Data;
+
+/**
+ * Created by Lars on 9/29/2014.
+ */
+public enum AbilityType {
+    JUMP,
+    STRENGTH,
+    SPEED
+
+
+}
--- core/src/com/FOS/Pixel/handlers/JsonHandler.java
+++ core/src/com/FOS/Pixel/handlers/JsonHandler.java
@@ -1,7 +1,10 @@
 package com.FOS.Pixel.handlers;
 
-import com.FOS.Pixel.LevelData;
+import com.FOS.Pixel.Data.AbilityParser;
+import com.FOS.Pixel.Data.AbilityType;
+import com.FOS.Pixel.Data.LevelData;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.utils.JsonReader;
 import com.badlogic.gdx.utils.OrderedMap;
@@ -12,21 +15,56 @@ import com.badlogic.gdx.utils.OrderedMap;
 
 
 public class JsonHandler {
-    private String JSON_PATH;
-    private Json json = new Json();
-    JsonReader jsonReader = new JsonReader();
+    private static final String JSON_PATH ="";
+    private static Json json = new Json();
+    private static JsonReader jsonReader = new JsonReader();
 
-    public JsonHandler(String path){
-        this.JSON_PATH = path;
+    public JsonHandler(String path){}
+
+    public static LevelData readLevel(int levelNumber){
+        OrderedMap<String,LevelData> jsonmap = json.fromJson(OrderedMap.class, Gdx.files.local("leveldata.json"));
+        return jsonmap.get("level"+levelNumber);
+    }
+
+    public static void test(){
+        AbilityParser test = new AbilityParser(1.1f,"jemoeder.png");
+        OrderedMap<String,OrderedMap<String,AbilityParser>> jsontest = new OrderedMap<String, OrderedMap<String, AbilityParser>>();
+        OrderedMap<String,AbilityParser> abilitymap = new OrderedMap<String, AbilityParser>();
+        abilitymap.put("Level1",test);
+        abilitymap.put("Level2",test);
+        abilitymap.put("Level3",test);
+        jsontest.put(AbilityType.SPEED.toString(), abilitymap);
+
+
+        jsontest.put(AbilityType.STRENGTH.toString(), abilitymap);
+        jsontest.put(AbilityType.JUMP.toString(), abilitymap);
+        String printdata = json.prettyPrint(jsontest);
+        FileHandle fileHandle = Gdx.files.local("abilitydata.json");
+        fileHandle.writeString(printdata, true);
 
     }
 
-//    public LevelData readLevel(int levelNumber){
-//        System.out.println(jsonReader.parse(Gdx.files.internal(JSON_PATH + "/leveldata.json")));
-//
-//        return  new LevelData();
-//    }
+    public  static AbilityParser getAbiltydata(AbilityType type, int level){
 
+        OrderedMap<String,OrderedMap<String,AbilityParser>> abilityTypeOrderedMap = json.fromJson(OrderedMap.class, Gdx.files.local("abilitydata.json"));
+        OrderedMap<String,AbilityParser> levelOrderMap = abilityTypeOrderedMap.get(type.toString());
+
+        return levelOrderMap.get("Level"+level);
+    }
 
 
 }
+
+class Abilitylevel{
+    String level;
+    AbilityParser abilityData;
+
+    Abilitylevel(String level, AbilityParser abilityData) {
+        this.level = level;
+        this.abilityData = abilityData;
+    }
+
+    Abilitylevel() {
+    }
+}
+
