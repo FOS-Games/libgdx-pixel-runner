7dd033afda53861c365b56ed0f038a9324cd2a4f
Stefan Orie <s.oorie@hotmail.com>
2014-10-06 16:48:08 +0200

    Start level 1 button !

--- core/src/com/FOS/Pixel/MainPixel.java
+++ core/src/com/FOS/Pixel/MainPixel.java
@@ -3,14 +3,17 @@ package com.FOS.Pixel;
 import com.FOS.Pixel.handlers.JsonHandler;
 import com.FOS.Pixel.handlers.SaveHandler;
 import com.FOS.Pixel.screens.GameScreen;
+import com.FOS.Pixel.screens.MainMenuScreen;
 import com.badlogic.gdx.Game;
 
 public class MainPixel extends Game {
 	@Override
-    public  void create(){
+    public void create(){
 
         SaveHandler.Initialize();
 
-        setScreen(new GameScreen(this,1));
+        //setScreen(new GameScreen(this,1));
+        setScreen(new MainMenuScreen(this));
+
     }
 }
--- core/src/com/FOS/Pixel/screens/MainMenuScreen.java
+++ core/src/com/FOS/Pixel/screens/MainMenuScreen.java
@@ -1,12 +1,92 @@
 package com.FOS.Pixel.screens;
 
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+
 /**
  * Created by Stefan on 6-10-2014.
  */
 public class MainMenuScreen extends MenuScreen {
 
+    Skin skin;
+    Stage stage;
+    SpriteBatch batch;
+    Game game;
+
+    public MainMenuScreen(Game game) {
+        this.game = game;
+    }
+
     @Override
     public void show() {
+        batch = new SpriteBatch();
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
 
+        skin = new Skin();
+
+        Pixmap pixmap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
+        pixmap.setColor(Color.WHITE);
+        pixmap.fill();
+        skin.add("white", new Texture(pixmap));
+
+        skin.add("default", new BitmapFont());
+
+        TextButtonStyle textButtonStyle = new TextButtonStyle();
+        textButtonStyle.up = skin.newDrawable("white", Color.DARK_GRAY);
+        textButtonStyle.down = skin.newDrawable("white", Color.DARK_GRAY);
+        textButtonStyle.checked = skin.newDrawable("white", Color.BLUE);
+        textButtonStyle.over = skin.newDrawable("white", Color.LIGHT_GRAY);
+        textButtonStyle.font = skin.getFont("default");
+        skin.add("default", textButtonStyle);
+
+        Table table = new Table();
+        table.setFillParent(true);
+        stage.addActor(table);
+
+        final TextButton bStart= new TextButton("Start level 1", skin);
+        table.add(bStart);
+
+        bStart.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                ((Game) Gdx.app.getApplicationListener()).setScreen(new GameScreen(game, 1));
+            }
+        });
+
+        //table.add(new Image(skin.newDrawable("white", Color.RED))).size(64);
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1/30f));
+        stage.draw();
+    }
+
+    @Override
+    public void resize (int width, int height) {
+        stage.getViewport().update(width, height, true);
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+        skin.dispose();
     }
 }
