0f18bf70d7cce5b8deeaf160273e1da49e42271c
Stefan Orie <s.oorie@hotmail.com>
2014-10-06 15:54:31 +0200

    Added screen classes

---
+++ core/src/com/FOS/Pixel/screens/MainMenuScreen.java
@@ -0,0 +1,12 @@
+package com.FOS.Pixel.screens;
+
+/**
+ * Created by Stefan on 6-10-2014.
+ */
+public class MainMenuScreen extends MenuScreen {
+
+    @Override
+    public void show() {
+
+    }
+}
