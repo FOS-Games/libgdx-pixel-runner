693e7270ebf4bddb36844439742b59b2d51a90df
Larsv94 <larsvolkers@hotmail.nl>
2014-10-09 09:09:21 +0200

    fixed texture + crate derp

--- core/src/com/FOS/Pixel/screens/GameScreen.java
+++ core/src/com/FOS/Pixel/screens/GameScreen.java
@@ -138,7 +138,7 @@ public class GameScreen extends PixelGameScreen {
     private void createBoxes() {
         ObjectMap<String,Body> bodies = parser.getBodies();
         for (ObjectMap.Entry<String,Body> x : bodies){
-            if(x.key.equals("crate")){
+            if(x.key.startsWith("crate")){
                 x.value.setUserData(new Box2DSprite(new Texture(Gdx.files.internal("obstacle.png"))));
                 for(Fixture fix: x.value.getFixtureList()){
                     fix.setSensor(true);
